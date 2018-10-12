f9f2355671e99ecead1b23c30832566e93d5cd1c
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-10-12 01:07:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,7 +17,7 @@ function init() {
   document.body.addEventListener('keydown', function(e) {
     const key = e.key;
     if (key === codes[i]) {
-      index++;
+      i++;
       if (i === codes.length) {
         alert('You did it!');
         index = 0;
